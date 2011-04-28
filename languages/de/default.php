b0e4bb5fd15d071dccf7d4b189531da0666711a7
Sven Rhinow <sven@sr-tag.de>
2011-04-28 12:32:31 +0200

    first commit

---
+++ languages/de/default.php
@@ -0,0 +1,36 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * TYPOlight webCMS
+ * Copyright (C) 2005-2009 Leo Feyer
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at http://www.gnu.org/licenses/.
+ *
+ * PHP version 5
+ * @copyright  Sven Rhinow 2009 
+ * @author     Sven Rhinow 
+ * @package    Language
+ * @license    LGPL 
+ * @filesource
+ */
+
+
+/**
+ * Miscellaneous
+ */
+$GLOBALS['TL_LANG']['MSC'][''] = '';
+$GLOBALS['TL_LANG']['FFL']['imagecaptcha']     = array('AntiSpam-Bild', 'ein Bild mit Zahlen , diese müssen in ein benachbartes Textfeld übertragen werden.');
+$GLOBALS['TL_LANG']['ERR']['imagecaptcha'] = 'Bitte tragen Sie die 5 stellige Nummer in das nebenstehende Feld';
+?>
